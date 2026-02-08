ac3e820d3b09d245838b8e46628a560396ffbcc7
ALEo14 <aleedin14@gmail.com>
2026-02-08 17:16:35 -0300

    arreglando la tabla de productos

--- db.py
+++ db.py
@@ -2,6 +2,9 @@
 conectar db
 """
 import sqlite3
+import os
+BASE_DIR =os.path.dirname(os.path.abspath(__file__))
+DB_NAME = os.path.join(BASE_DIR, "database.db")
 
 DB_NAME = 'database.db'
 def conectar():
